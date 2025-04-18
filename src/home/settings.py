873dae0123dd2fd0992e9aec424b6f4ebb0d028d
Elie <niyubwayoiraelie5777@gmail.com>
2025-04-18 22:08:48 +0200

    Prepare for Railway

--- src/home/settings.py
+++ src/home/settings.py
@@ -13,7 +13,12 @@ SECRET_KEY = 'django-insecure-^!fp5c@e=48a!v5eih26u391vriksbnhrtvn_k82up%w05fp==
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app'
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- src/home/urls.py
+++ src/home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page_view
+from .views import home_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_page_view, name='home'),
+    path("", home_view),
+    path('home/', home_view, name='home'),
+    path("about/", about_view),
 ]
